d5a5ca81fa55bcf7336af6a4e0769f11a09238b2
Binh Nguyen <binh@endpointclosing.com>
2023-03-20 20:30:40 -0700

    add dynamic text colors

--- app/test-component.tsx
+++ app/test-component.tsx
@@ -1,42 +1,46 @@
 'use client';
 
-import { createStyles, Select, TextInput } from '@mantine/core';
+import { createStyles, Text, Title, Button } from '@mantine/core';
+import { useState } from 'react';
 
-const useStyles = createStyles((theme) => ({
+// Use createStyles for static extraction / className approach
+// A solution would be to expose the createStyles function for client-apps to use
+const useStyles = createStyles((theme, props) => ({
   root: {
     position: 'relative',
   },
-
   input: {
     height: 'auto',
     paddingTop: 18,
   },
-
   label: {
     position: 'absolute',
     pointerEvents: 'none',
-    fontSize: theme.fontSizes.xs,
     paddingLeft: theme.spacing.sm,
     paddingTop: theme.spacing.sm / 2,
     zIndex: 1,
   },
+  dynamicText: {
+    // @ts-ignore
+    color: props?.textColor === 'red' ? 'red' : 'blue',
+  },
 }));
 
 export function Test() {
+  const [textColor, setTextColor] = useState('red');
   // You can add these classes as classNames to any Mantine input, it will work the same
-  const { classes } = useStyles();
+  // @ts-ignore
+  const { classes } = useStyles({ textColor });
+
+  const handleButtonClick = () => {
+    setTextColor(textColor === 'red' ? 'blue' : 'red');
+  };
 
   return (
-    <div>
-      <TextInput label="Shipping address" placeholder="15329 Huston 21st" classNames={classes} />
-
-      <Select
-        style={{ marginTop: 20, zIndex: 2 }}
-        data={['React', 'Angular', 'Svelte', 'Vue']}
-        placeholder="Pick one"
-        label="Your favorite library/framework"
-        classNames={classes}
-      />
+    <div style={{ padding: '20px' }}>
+      <Title>This is a NextJS header</Title>
+      <Text className={classes.dynamicText}>Dynamic text color: {textColor}</Text>
+      <Button onClick={handleButtonClick}>Change text color</Button>
     </div>
   );
 }
